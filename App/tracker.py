b44ed80106e570ea67b85da50a66ed79da0c28b3
iLa <nabilaakbars1@gmail.com>
2025-09-10 14:36:13 +0800

    add frame kategori

--- App/tracker.py
+++ App/tracker.py
@@ -8,9 +8,19 @@ tinggi =600
 #x =500
 #y =100
 
+#judul dan nama aplikasi
 window.title("Expense Tracker")
 label_judul = tk.Label(window, text="Aplikasi Catatan Keuangan Sederhana 𐔌՞꜆. ̫.꜀՞𐦯", font=("Product Sans", 16, "bold"))
 label_judul.pack(pady=20)
+
+#frame inputan
+frame_input = tk.Frame(window)
+frame_input.pack(pady=8)
+
+tk.Label(frame_input, text="Kategori:").grid(row=0, column=0, padx=5, pady=5)
+entry_kategori = tk.Entry(frame_input)
+entry_kategori.grid(row=0, column=1, padx= 10, pady=10)
+
 
 #Mengatur windownya akan muncul ditengah layar
 screenwidth = window.winfo_screenwidth()
