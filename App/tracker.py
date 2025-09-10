8b4fb4671af7c5589f392694078adb20074a8608
iLa <nabilaakbars1@gmail.com>
2025-09-10 14:17:28 +0800

    atur ukuran dan nama

--- App/tracker.py
+++ App/tracker.py
@@ -1,15 +1,18 @@
 import tkinter as tk
+from tkinter import ttk, messagebox
 
 #Membuat windownya dan atur ukurannya
-window= tk.Tk()
+window = tk.Tk()
 lebar =600
 tinggi =600
-x =500
-y =100
+#x =500
+#y =100
 
-window.title("Expense Tracker") 
+window.title("Expense Tracker")
+label_judul = tk.Label(window, text="Aplikasi Catatan Keuangan Sederhana ˙𐃷˙", font=("Product Sans", 16, "bold"))
+label_judul.pack(pady=20)
 
-#Mengatur windownya akan muncul ditengah
+#Mengatur windownya akan muncul ditengah layar
 screenwidth = window.winfo_screenwidth()
 screenheight = window.winfo_screenheight()
 newx = int((screenwidth/2) - (lebar/2))
